cb24729b49c7f61502920caa09f75b051c0fb966
hasancennet73-gif <hasancennet73@gmail.com>
2026-04-18 17:16:04 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -8,12 +7,10 @@ const firebaseConfig = {
   projectId: "sevrawms",
   storageBucket: "sevrawms.firebasestorage.app",
   messagingSenderId: "828095391902",
-  appId: "1:828095391902:web:6b358646d69059901ef38f",
-  measurementId: "G-1YP4WHJWBP"
+  appId: "1:828095391902:web:6b358646d69059901ef38f"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { app, analytics, db };
+export { db };
